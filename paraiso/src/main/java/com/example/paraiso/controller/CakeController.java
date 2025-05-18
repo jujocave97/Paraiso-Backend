18e63db8f48cf732d18fe72208e40b12f3de7357
JJ <jujocave97@gmail.com>
2025-05-18 23:52:36 +0200

    little changes

--- paraiso/src/main/java/com/example/paraiso/controller/CakeController.java
+++ paraiso/src/main/java/com/example/paraiso/controller/CakeController.java
@@ -1,15 +1,10 @@
 package com.example.paraiso.controller;
 
 import com.example.paraiso.dto.CakeDTO;
-import com.example.paraiso.model.Cake;
-import com.example.paraiso.model.EstadoReserva;
-import com.example.paraiso.model.ReservaCake;
-import com.example.paraiso.repository.CakeRepository;
 import com.example.paraiso.service.AuthService;
 import com.example.paraiso.service.CakeService;
 import com.example.paraiso.service.ReservaCakeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
