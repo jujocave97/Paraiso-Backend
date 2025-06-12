c989296db85ff3dadae5c2f9bc2f513308e71b4d
JJ <jujocave97@gmail.com>
2025-06-12 19:49:10 +0200

    cors application

--- paraiso/src/main/java/com/example/paraiso/config/SecurityConfig.java
+++ paraiso/src/main/java/com/example/paraiso/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(cors -> {})
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
